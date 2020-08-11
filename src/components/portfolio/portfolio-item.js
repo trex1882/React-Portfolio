0bb8613552e3a80997b4642f3fac9849fdd50be8
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-08-11 14:13:19 -0600

    comit

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -20,11 +20,28 @@ export default class PortfolioItem extends Component {
   render() {
     const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
-        <div>
-            <img src={thumb_image_url} />
-            <img src={logo} />
-            <div>{description}</div>
-            <Link to={`/portfolio/${id}`}>Link</Link>
+      <div
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
+        <div
+          className={
+            "portfolio-img-background " + this.state.portfolioItemClass
+          }
+          style={{
+            backgroundImage: "url(" + thumb_image_url + ")"
+          }}
+        />
+
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} />
+          </div>
+
+          <div className="subtitle">{description}</div>
         </div>
+      </div>
     );
+  }
 }
