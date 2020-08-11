68428dfa59234669a6611698a5fa0ea6bdcce3d2
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-08-11 15:29:02 -0600

    began blog

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    return (
       <div>
-        <Link to="/about-me">Read more about myself</Link>
+        <h2>Blog</h2>
+
+        <div>
+          <Link to="/about-me">Read more about myself</Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
