d8e5ffb3d239f0f9df7722c896115e77b0e3c5bf
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-08-11 14:13:15 -0600

    comit

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
 
-export default function(props) {
+export default class PortfolioItem extends Component {
+  constructor(props) {
+    super(props);
 
-    const {  id, description, thumb_image_url, logo} = props.item;
+    this.state = {
+      portfolioItemClass: ""
+    };
+  }
+
+  handleMouseEnter() {
+    this.setState({ portfolioItemClass: "image-blur" });
+  }
+
+  handleMouseLeave() {
+    this.setState({ portfolioItemClass: "" });
+  }
+
+  render() {
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
         <div>
             <img src={thumb_image_url} />
