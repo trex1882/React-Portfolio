7ec69e93e4b297fa5f802c68b77ed2b3a0550f70
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-08-11 20:37:48 -0600

    final spinner

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
 
