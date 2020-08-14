f50e1483b3dc1a8d5beb42a4180fd181558ddb7a
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-08-13 23:48:16 -0600

    final late night comit

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -10,10 +10,18 @@ export default class BlogForm extends Component {
     this.state = {
       title: "",
       blog_status: "",
+      content: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(
+      this
+    );
+  }
+
+  handleRichTextEditorChange(content) {
+    this.setState({ content });
   }
 
   buildForm() {
@@ -21,6 +29,7 @@ export default class BlogForm extends Component {
 
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append("portfolio_blog[content]", this.state.content);
 
     return formData;
   }
@@ -33,14 +42,15 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleSuccessfullFormSubmission(
-          response.data.portfolio_blog
-        );
-
         this.setState({
           title: "",
           blog_status: "",
+          content: "",
         });
+
+        this.props.handleSuccessfullFormSubmission(
+          response.data.portfolio_blog
+        );
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);
@@ -77,7 +87,9 @@ export default class BlogForm extends Component {
         </div>
 
         <div className="one-column">
-          <RichTextEditor />
+          <RichTextEditor
+            handleRichTextEditorChange={this.handleRichTextEditorChange}
+          />
         </div>
 
         <button className="btn">Save</button>
--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import draftTohtml from "draftjs-to-html";
+import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
 
 export default class RichTextEditor extends Component {
@@ -11,14 +11,27 @@ export default class RichTextEditor extends Component {
     this.state = {
       editorState: EditorState.createEmpty(),
     };
+
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
   }
+
+  onEditorStateChange(editorState) {
+    this.setState(
+      { editorState },
+      this.props.handleRichTextEditorChange(
+        draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+      )
+    );
+  }
+
   render() {
     return (
       <div>
         <Editor
           editorState={this.state.editorState}
           wrapperClassName="demo-wrapper"
-          editorClassName="demo-editor"
+          editorClassname="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
         />
       </div>
     );
